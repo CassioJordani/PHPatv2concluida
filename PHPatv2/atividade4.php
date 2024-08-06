e8665009cbfc17213d15ac83270e4630c2a94064
CassioJordani <141282305+CassioJordani@users.noreply.github.com>
2024-08-06 16:04:55 -0300

    Update atividade4.php

--- PHPatv2/atividade4.php
+++ PHPatv2/atividade4.php
@@ -1,20 +1,16 @@
 <?php
 // Modifique a classe 'Cachorro' do exercício anterior.
 // Torne as propriedades 'nome' e 'idade' protegidas e utilize métodos getters e setters para acessá-las e modificá-las.
-// Definição da classe base Animal
 
-// Classe base Animal com propriedades protegidas
 class Animal {
     protected $nome;
     protected $idade;
 
-    // Construtor
     public function __construct($nome, $idade) {
         $this->nome = $nome;
         $this->idade = $idade;
     }
 
-    // Métodos getters e setters
     public function getNome() {
         return $this->nome;
     }
@@ -32,12 +28,9 @@ class Animal {
     }
 }
 
-// Classe derivada Cachorro com getters e setters herdando de Animal
 class Cachorro extends Animal {
-    // Sobrescrever métodos caso necessário
 }
 
-// Criando um objeto da classe Cachorro
 $cachorro = new Cachorro("Rex", 5);
 echo "Nome do cachorro: " . $cachorro->getNome() . "<br>";
 echo "Idade do cachorro: " . $cachorro->getIdade();
